491aabe281e9ee094c764c4de5d51f07977314f8
NateTheDev1 <nthnlrichards@gmail.com>
2020-07-16 20:57:06 -0400

    Initial Setup W/o db

--- index.js
+++ index.js
@@ -1,24 +1,21 @@
-// allows you to read the environment variable from the .env file
-require('dotenv').config()
-// imports the server file in 
-const server = require('./server/server.js')
-// allows for custom color output in the terminal
-const colors = require('colors')
-// logs api call information to the terminal
-const morgan = require('morgan')
+require("dotenv").config();
 
-// define environment variables for port and environment
-const PORT = process.env.PORT
-const ENV = process.env.NODE_ENV
+const server = require("./server/server");
 
-// this turns on api call logger, only if in development.
-if(ENV === 'development'){
-    server.use(morgan('dev'))
+const colors = require("colors");
+
+const morgan = require("morgan");
+
+const PORT = process.env.PORT;
+const ENV = process.env.NODE_ENV;
+
+if (ENV === "development") {
+  server.use(morgan("dev"));
 }
 
-// this turs on the server. ensures the server is listening on the defined port
-// its important to make sure that the port is a variable and not hardcoded in.
-// when you deploy to heroku, they will assign ports based on their availability
 server.listen(PORT, () => {
-    console.log(`\n ===== Server running in ${ENV} mode on port ${PORT} ===== \n`.magenta.bold.underline)
-})
+  console.log(
+    `\n ===== Server running in ${ENV} mode on port ${PORT} ===== \n`.magenta
+      .bold.underline
+  );
+});
